38244a97101796e75e495157b3eb09b90fe56809
yunus emre demir <yunus.emredemir222002@gmail.com>
2019-04-09 00:42:30 +0300

    Create Araba5.java

---
+++ Ders_YTU_2019/Calismalar_5/Araba5.java
@@ -0,0 +1,16 @@
+package Calismalar_5;
+
+public class Araba5 {
+	private String plaka;
+	
+	public Araba5(String plaka) {
+		this.plaka=plaka;
+	}
+	
+	public String getPlaka() {
+		return plaka;
+	}
+	
+	
+	
+}
